648116879d6dbe2a26919458e050c26e0c6e0f47
ªâãåí Òóêóáàºâ <yevhen.tukubaiev@nure.ua>
2022-10-03 02:02:38 +0300

    Add COM initialization.

--- spz_lb02_9/main.c
+++ spz_lb02_9/main.c
@@ -1,7 +1,50 @@
 #define _WIN32_DCOM
 #include "pch.h"
 
-int main(int argc, char **argv)
+HRESULT CoWMIInit(VOID);
+
+VOID CoWMIUninit(VOID);
+
+HRESULT CoWMIInit(VOID)
 {
-	return 0;
+	HRESULT hr = S_OK;
+	
+	hr = CoInitializeEx(NULL, COINIT_MULTITHREADED);
+	if (FAILED(hr)) {
+		_tprintf_s(_T("CoInitializeEx failed, hr: %X\n"), hr);
+		return hr;
+	}
+
+	hr = CoInitializeSecurity(
+		NULL,                        // Security descriptor    
+		-1,                          // COM negotiates authentication service
+		NULL,                        // Authentication services
+		NULL,                        // Reserved
+		RPC_C_AUTHN_LEVEL_DEFAULT,   // Default authentication level for proxies
+		RPC_C_IMP_LEVEL_IMPERSONATE, // Default Impersonation level for proxies
+		NULL,                        // Authentication info
+		EOAC_NONE,                   // Additional capabilities of the client/server
+		NULL                         // Reserved
+	);
+	if (FAILED(hr)) {
+		_tprintf_s(_T("CoInitializeSecurity failed, hr: %X\n"), hr);
+		return hr;
+	}
+
+	return S_OK;
+}
+
+VOID CoWMIUninit(VOID)
+{
+	CoUninitialize();
+	return;
+}
+
+int _tmain(int argc, TCHAR **argv)
+{
+	HRESULT hr = CoWMIInit();
+	if (SUCCEEDED(hr)) {
+	}
+	CoWMIUninit();
+	return (int)hr;
 }
--- spz_lb02_9/pch.h
+++ spz_lb02_9/pch.h
@@ -1,3 +1,6 @@
 #pragma once
 #include <stdio.h>
+#include <tchar.h>
 #include <wbemidl.h>
+
+#pragma comment(lib, "wbemuuid.lib")
