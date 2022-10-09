4a117f2987ed6b5ca1b3f1973e6eaf734fc4ca25
ªâãåí Òóêóáàºâ <yevhen.tukubaiev@nure.ua>
2022-10-09 14:15:48 +0300

    Fix getter loop unneccesary iteration & refactor.

--- spz_lb02_9/task01.c
+++ spz_lb02_9/task01.c
@@ -15,7 +15,8 @@ HRESULT Task01(VOID)
 	hr = pSvc->ExecQuery(
 		(BSTR)_T("WQL"),
 		(BSTR)_T(
-			"SELECT * FROM Win32_BIOS"
+			"SELECT * "
+			"FROM Win32_BIOS"
 		),
 		WBEM_FLAG_FORWARD_ONLY
 		| WBEM_FLAG_RETURN_IMMEDIATELY,
@@ -23,21 +24,23 @@ HRESULT Task01(VOID)
 		&pEnumerator
 	);
 
-	ULONG uRet = 0;
+	
 	while (pEnumerator) {
+		ULONG uRet = 0;
 		hr = pEnumerator->Next(WBEM_INFINITE, 1, &pClsObj, &uRet);
 		if (uRet == 0)
 			break;
 		VARIANT vt;
 		VariantInit(&vt);
-		for (LPCTSTR *prop = taskProps; prop && SUCCEEDED(hr); prop++) {
-			hr = pClsObj->Get(
+		for (LPCTSTR *prop = taskProps; prop; prop++) {
+			HRESULT get_res = pClsObj->Get(
 				*prop,
 				0,
 				&vt,
 				0,
 				0
 			);
+			if (FAILED(get_res)) break;
 			_tprintf_s(_T("%s: %s\n"), *prop, vt.bstrVal);
 		}
 		VariantClear(&vt);
