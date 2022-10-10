74bdff96e5299cb59c9e7353faf97eaece60acd1
ªâãåí Òóêóáàºâ <yevhen.tukubaiev@nure.ua>
2022-10-10 16:22:51 +0300

    Implement getting available properties for the task01.

--- spz_lb02_9/task01.h
+++ spz_lb02_9/task01.h
@@ -1,3 +1,7 @@
 #include "pch.h"
 
 HRESULT Task01(VOID);
+/* Get all available properties for the given class */
+static HRESULT Task01_GetPropertyInfo(VOID);
+/* Get property values for our task */
+static HRESULT Task01_GetIndividualProperties(VOID);
